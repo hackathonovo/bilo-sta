74cee397d257e2b49b3364d2529585e3cf88133a
MSekrst <matija.sekrst13@gmail.com>
2017-05-20 23:06:47 +0200

    removed action

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -30,38 +30,41 @@ export default class ListItem extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {};
+    this.state = { data: this.props.data };
 
     this.handleInputChange = this.handleInputChange.bind(this);
   }
 
   handleInputChange(e) {
-    console.log(e);
+    const newState = { ...this.state };
+    newState.data[e.target.name] = e.target.value;
+
+    this.setState(newState);
   }
 
   render() {
-    console.log(this.props.data);
-    const data = this.props.data;
+    const data = this.state.data;
+    console.log(data);
 
     return (
       <div className="card height-fix cursor">
-        <div className="card-header" role="tab" id={data._id}>
-          <h5 className="mb-0">
-            <HiddenLink data-toggle="collapse" data-parent="#people" href={`#colapse-${data._id}`} aria-expanded="true" aria-controls={`colapse-${data._id}`}>
+        <HiddenLink data-toggle="collapse" data-parent="#people" href={`#colapse-${data._id}`} aria-expanded="true" aria-controls={`colapse-${data._id}`}>
+          <div className="card-header" role="tab" id={data._id}>
+            <h5 className="mb-0">
               {`${data.firstname} ${data.lastname}`}
-            </HiddenLink>
-          </h5>
-        </div>
+            </h5>
+          </div>
+        </HiddenLink>
 
         <div id={`colapse-${data._id}`} className="collapse" role="tabpanel" aria-labelledby={data._id}>
           <div className="card-block">
-            <FieldContainer><Label>Ime:&nbsp;</Label><Input type="text" value={data.firstname} onChange={this.handleInputChange} /></FieldContainer>
-            <FieldContainer><Label>Prezime:&nbsp;</Label><Input type="text" value={data.lastname} onChange={this.handleInputChange} /></FieldContainer>
-            <FieldContainer><Label>E-mail:&nbsp;</Label><Input type="text" value={data.mail} onChange={this.handleInputChange} /></FieldContainer>
-            <FieldContainer><Label>Broj mobitela&nbsp;</Label><Input type="text" value={data.phoneNumber} onChange={this.handleInputChange} /></FieldContainer>
-            <FieldContainer><Label>Specijalnosti&nbsp;</Label><Input type="text" value={data.profession} onChange={this.handleInputChange} /></FieldContainer>
-            <FieldContainer><Label>Uloga&nbsp;</Label><Input type="text" value={data.role} onChange={this.handleInputChange} /></FieldContainer>
-            <FieldContainer><Label>Ima smartphone&nbsp;</Label><Input type="checkbox" checked data-toggle="toggle" onChange={this.handleInputChange} /></FieldContainer>
+            <FieldContainer><Label>Ime:&nbsp;</Label><Input name="firstname" type="text" value={data.firstname} onChange={this.handleInputChange} /></FieldContainer>
+            <FieldContainer><Label>Prezime:&nbsp;</Label><Input name="lastname" type="text" value={data.lastname} onChange={this.handleInputChange} /></FieldContainer>
+            <FieldContainer><Label>E-mail:&nbsp;</Label><Input name="mail" type="text" value={data.mail} onChange={this.handleInputChange} /></FieldContainer>
+            <FieldContainer><Label>Broj mobitela&nbsp;</Label><Input name="phoneNumber" type="text" value={data.phoneNumber} onChange={this.handleInputChange} /></FieldContainer>
+            {/*<FieldContainer><Label>Specijalnosti&nbsp;</Label><Input type="text" value={data.profession} onChange={this.handleInputChange} /></FieldContainer>*/}
+            {/*<FieldContainer><Label>Uloga&nbsp;</Label><Input type="text" value={data.role} onChange={this.handleInputChange} /></FieldContainer>*/}
+            {/*<FieldContainer><Label>Ima smartphone&nbsp;</Label><Input type="checkbox" checked data-toggle="toggle" onChange={this.handleInputChange} /></FieldContainer>*/}
           </div>
         </div>
       </div>
