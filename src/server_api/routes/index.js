c2fb070c9b0f1d27df9e9ae67ffa967c86796c69
erikm0111 <erikmatosevic@gmail.com>
2017-05-20 17:51:07 +0200

    fix

--- src/server_api/routes/index.js
+++ src/server_api/routes/index.js
@@ -3,6 +3,6 @@ var router = express.Router();
 var controllerPeople = require('../controllers/controllerPeople');
 
 router.post('/login', controllerPeople.login);
-
+router.post('/people', controllerPeople.createOne);
 
 module.exports = router;
