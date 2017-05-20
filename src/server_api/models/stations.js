c0748e99f67bfdb746062451af35bd0ed0b89c51
LukaKaic <luka.kaic@fer.hr>
2017-05-20 15:15:40 +0200

    dodani modeli za bazu

---
+++ src/server_api/models/stations.js
@@ -0,0 +1,19 @@
+/**
+ * Created by Matija on 20.5.2017..
+ */
+
+var mongoose = require('mongoose');
+var locationSchema = require('./locations')
+
+var stationSchema = new mongoose.Schema({
+  name :{type : String, required : true},
+  location : {type : locationSchema, required:true}
+});
+
+mongoose.model('Station', stationSchema);
+
+
+
+module.exports = {
+  stationSchema : stationSchema
+};
--- src/server_api/routes/index.js
+++ src/server_api/routes/index.js
@@ -6,4 +6,5 @@ router.get('/locations', function(req, res){
     res.send("skdfjk");
 });
 
+
 module.exports = router;
