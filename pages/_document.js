1cf8dde13f6cb041398a234f14801fa3cc4b8fc8
MSekrst <matija.sekrst13@gmail.com>
2017-05-21 01:45:44 +0200

    spasitelj

--- pages/_document.js
+++ pages/_document.js
@@ -11,7 +11,7 @@ export default class MyDocument extends Document {
           <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossOrigin="anonymous" />
           <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossOrigin="anonymous" />
           <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossOrigin="anonymous" />
-          <link href="https://fonts.googleapis.com/css?family=Lato&amp;subset=latin-ext" rel="stylesheet" />
+          <link href="https://fonts.googleapis.com/css?family=Lato:400,700&amp;subset=latin-ext" rel="stylesheet" />
           <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossOrigin="anonymous" />
           <link rel="stylesheet" href="static/css/style.css" />
         </Head>
--- src/components/AppContainer.js
+++ src/components/AppContainer.js
@@ -6,7 +6,7 @@ import Content from './Content';
 const adminItems = {
   Raspoloživost: { subItems: ['Novi unos', 'Pregled'], icon: 'availability.png' },
   Lokacija: { subItems: ['Novi unos', 'Pregled'], icon: 'location.png' },
-  Spašavatelji: { subItems: ['Novi unos', 'Uređivanje'], icon: 'profile.png' },
+  Spasitelj: { subItems: ['Novi unos', 'Uređivanje'], icon: 'profile.png' },
   Akcije: { subItems: ['Uređivanje', 'Pregled'] , icon: 'action.png' },
   Šifrarnici: { subItems: ['Novi unos', 'Pregled'], icon: 'key.png' },
 };
