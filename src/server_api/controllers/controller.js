2ff59f098f7ffef6f02066ab83d9eb82796caeaa
erikm0111 <erikmatosevic@gmail.com>
2017-05-20 16:22:12 +0200

    pwd validation person

--- src/server_api/controllers/controller.js
+++ src/server_api/controllers/controller.js
@@ -11,22 +11,22 @@ module.exports.login = function(req, res){
   if (req.body.username || req.body.password){
     var username = req.body.username;
     var password = req.body.password;
-    console.log(username, password);
-    Person.find({username:username}, function (err, person) {
-      console.log(person);
-      if (!person) {
-        sendJsonResponse(res, 400, {"message":"person not found"});
-      } if (err){
+    Person.findOne({username:username}, function (err, person) {
+      if (err){
         sendJsonResponse(res, 400, err);
       } else {
         console.log(person);
-        sendJsonResponse(res, 200, person);
+        if (password === person.password){
+          sendJsonResponse(res, 200, person);
+        } else {
+          sendJsonResponse(res, 400, {"message":"Invalid password"});
+        }
       }
     });
     // var hashStr = crypto.createHash('md5').update(username).digest('hex');
     // var roleStr = 'role';
     // var content = {
-    //   token: hashStr,
+    //   token: hashStr
     //   role: roleStr
     // };
     // sendJsonResponse(res, 200, content);
